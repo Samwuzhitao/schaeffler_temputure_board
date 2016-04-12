d4d390b6de6ede2d3774c3e254416329a5a79539
Samwuzhitao <1322673636@qq.com>
2016-04-12 20:33:15 +0800

    Source:Commit:First Commit
    Signed-off-by: Samwuzhitao <1322673636@qq.com>

---
+++ TMS570LS1227_DEMO/App/CanProtocol.c
@@ -0,0 +1,380 @@
+/*
+ * CanProtocol.c
+ *
+ *  Created on: 2016Äê3ÔÂ29ÈÕ
+ *      Author: Sam
+ */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#include "stdio.h"
+#include "sys_common.h"
+#include "can.h"
+#include "CanProtocol.h"
+
+#define CANRINGBUFFERLEN                   10
+
+/* Private variables ---------------------------------------------------------*/
+/* can peripheral variables */
+uint8 CanSetSrcAddr = CAN_SRC_ADDR;
+uint8 CanRxCmdTopCounter = 0;
+uint8 CanRxCmdButtomCounter = 0;
+uint8 CanReceivecompleteFlg = 0;
+
+CAN_MessageTypedef CanRxCmdRingBuffer[CANRINGBUFFERLEN];
+
+
+void cansetIDType(canBASE_t *node, uint32 messageBox, uint8 IdType)
+{
+    /** - Wait until IF2 is ready for use */
+    while ((node->IF2STAT & 0x80U) ==0x80U)
+    {
+    } /* Wait */
+
+	/** - Configure IF2 for
+	*     - Message direction - Read
+	*     - Data Read
+	*     - Clears NewDat bit in the message object.
+	*/
+	node->IF2CMD = 0xA0U;
+	/* Copy passed value into the arbitration register. */
+	node->IF2MSK |= 0x80000000;
+
+	 if(IdType == CAN_ID_EXT)
+	 {
+		 node->IF2ARB |= 0x40000000;
+	 }
+	 else
+	 {
+	     node->IF2ARB &= ~0x40000000;
+	 }
+
+    /** - Update message box number. */
+    /*SAFETYMCUSW 93 S MR: 6.1,6.2,10.1,10.2,10.3,10.4 <APPROVED> "LDRA Tool issue" */
+    node->IF2NO = (uint8) messageBox;
+
+    /** - Wait until data are copied into IF2 */
+    while ((node->IF2STAT & 0x80U) ==0x80U)
+    {
+    } /* Wait */
+}
+
+uint8 canGetIDType(canBASE_t *node, uint32 messageBox)
+{
+    uint32 IdType;
+
+    /** - Wait until IF2 is ready for use */
+    while ((node->IF2STAT & 0x80U) ==0x80U)
+    {
+    } /* Wait */
+
+	/** - Configure IF2 for
+	*     - Message direction - Read
+	*     - Data Read
+	*     - Clears NewDat bit in the message object.
+	*/
+	node->IF2CMD = 0xA0U;
+	/* Copy passed value into the arbitration register. */
+	IdType = (node->IF2ARB)& 0x40000000;
+    /** - Update message box number. */
+    /*SAFETYMCUSW 93 S MR: 6.1,6.2,10.1,10.2,10.3,10.4 <APPROVED> "LDRA Tool issue" */
+    node->IF2NO = (uint8) messageBox;
+
+    /** - Wait until data are copied into IF2 */
+    while ((node->IF2STAT & 0x80U) ==0x80U)
+    {
+    } /* Wait */
+
+    if(IdType)
+    	return CAN_ID_EXT;
+    else
+    	return CAN_ID_STD;
+}
+
+void canSetID(canBASE_t *node, uint32 messageBox, uint32 msgBoxArbitVal)
+{
+
+    /** - Wait until IF2 is ready for use */
+    while ((node->IF2STAT & 0x80U) ==0x80U)
+    {
+    } /* Wait */
+
+	/** - Configure IF2 for
+	*     - Message direction - Read
+	*     - Data Read
+	*     - Clears NewDat bit in the message object.
+	*/
+	node->IF2CMD = 0xA0U;
+	/* Copy passed value into the arbitration register. */
+	//node->IF2ARB &= 0x80000000U;
+	//node->IF2ARB |= (msgBoxArbitVal & 0x7FFFFFFFU);
+	node->IF2ARB  = (uint32)0x80000000U | (uint32)0x00000000U | (uint32)0x20000000U | msgBoxArbitVal ;
+
+    /** - Update message box number. */
+    /*SAFETYMCUSW 93 S MR: 6.1,6.2,10.1,10.2,10.3,10.4 <APPROVED> "LDRA Tool issue" */
+    node->IF2NO = (uint8) messageBox;
+
+    /** - Wait until data are copied into IF2 */
+    while ((node->IF2STAT & 0x80U) ==0x80U)
+    {
+    } /* Wait */
+
+}
+/******************************************************************************
+  Function: CAN_Transmit
+  Description:
+  Input :
+  	  node       :node Pointer to CAN node
+ 	  messageBox :messageBox Message box number of CAN node
+ 	  pMessage   :Can need to transmit message
+  Output:
+  Return: The function will return:
+           - 0: When the setup of the TX message box wasn't successful
+           - 1: When the setup of the TX message box was successful
+  Others:None
+******************************************************************************/
+static uint32 CAN_Transmit(canBASE_t *node, uint32 messageBox, CanMsg *pMessage )
+{
+	CAN_EXTSTDIDTypedef CanId;
+	uint32 status;
+
+	if(pMessage->IDE == CAN_ID_STD)
+	{
+		CanId.Id = pMessage->StdId;
+		cansetIDType(canREG3, 1, CAN_ID_STD);
+	}
+	else
+	{
+		CanId.Id = pMessage->ExtId;
+		cansetIDType(canREG3, 1, CAN_ID_EXT);
+	}
+
+	canSetID(canREG3, 1, CanId.Id);
+
+	status = canTransmit( node, messageBox, pMessage->Data );
+
+	//printf("Massage box%2d id = %8x idtype = %1d canTransmit return status = %4d \r\n", 1, tmpid, tmpIdType, status);
+
+	return status;
+
+}
+
+/******************************************************************************
+  Function:CAN_MessageGet
+  Description:
+  Input:None
+  Output:
+  Return:
+  Others:None
+******************************************************************************/
+void CAN_MessageGet( CanMsg *pMessage )
+{
+	int i;
+	uint8 cmd;
+	CAN_STDEXTIDAddrTypedef CanSrcAddr,CanDstAddr;
+	CAN_EXTSTDIDTypedef CanId;
+
+	if( pMessage->IDE  == CAN_ID_STD )
+	{
+		CanId.Id = pMessage->StdId;
+		CanDstAddr.CanIDAddr.StdIDAddr = CanId.CanStdId.DstAddr;
+		CanSrcAddr.CanIDAddr.StdIDAddr = CanId.CanStdId.SrcAddr;
+		cmd = CanId.CanStdId.FunCode;
+	}
+	else
+	{
+		CanId.Id = pMessage->ExtId;
+		CanDstAddr.CanIDAddr.StdIDAddr = CanId.CanExtId.DstAddr;
+		CanSrcAddr.CanIDAddr.StdIDAddr = CanId.CanExtId.SrcAddr;
+		CanDstAddr.CanIDAddr.ExtIDAddr = CanId.CanExtId.ExtDstAddr;
+		CanSrcAddr.CanIDAddr.ExtIDAddr = CanId.CanExtId.ExtSrcAddr;
+		cmd = CanId.CanExtId.FunCode;
+	}
+
+	printf("CanId.id.FunCode: %x\r\n",(cmd) & 0x1f);
+	printf("CanId.id.SrcAddr: %x\r\n",CanSrcAddr.IDAddr );
+	printf("CanId.id.DstAddr: %x\r\n",CanDstAddr.IDAddr);
+
+	if( CanDstAddr.IDAddr == CanSetSrcAddr  )
+	{
+		CanRxCmdRingBuffer[CanRxCmdTopCounter].SrcAddr = CanSrcAddr.IDAddr;
+		CanRxCmdRingBuffer[CanRxCmdTopCounter].DstAddr = CanDstAddr.IDAddr;
+		CanRxCmdRingBuffer[CanRxCmdTopCounter].CanCmd = ( cmd ) & 0x1f;
+		CanRxCmdRingBuffer[CanRxCmdTopCounter].IdType = pMessage->IDE;
+
+		for( i=0; i<8; i++)
+		{
+			CanRxCmdRingBuffer[CanRxCmdTopCounter].data[i] = pMessage->Data[i];
+		}
+
+		CanReceivecompleteFlg = 1;
+
+		CanRxCmdTopCounter++;
+
+		if(CanRxCmdTopCounter == CANRINGBUFFERLEN)
+		{
+			CanRxCmdTopCounter = 0;
+		}
+	}
+}
+
+/******************************************************************************
+  Function:Can_to_can_change_id
+  Description:
+  Input:None
+  Output:
+  Return:
+  Others:None
+******************************************************************************/
+void Can_change_return_id( CanMsg *pMessage, uint8_t cmd )
+{
+
+	CAN_EXTSTDIDTypedef MyCAN_ID;
+	CAN_STDEXTIDAddrTypedef MySrcIDAddr,MyDstIDAddr;
+
+	MyDstIDAddr.IDAddr = CanRxCmdRingBuffer[CanRxCmdButtomCounter].SrcAddr;
+	MySrcIDAddr.IDAddr = CanRxCmdRingBuffer[CanRxCmdButtomCounter].DstAddr;
+
+	if(  CanRxCmdRingBuffer[CanRxCmdButtomCounter].IdType == CAN_ID_STD )
+	{
+		MyCAN_ID.CanStdId.DstAddr = MyDstIDAddr.CanIDAddr.StdIDAddr ;
+		MyCAN_ID.CanStdId.SrcAddr = MySrcIDAddr.CanIDAddr.StdIDAddr ;
+		MyCAN_ID.CanStdId.FunCode = cmd | 0x10;
+		/*
+		printf("CanId.id.FunCode: %x\r\n",MyCAN_ID.CanStdId.FunCode);
+		printf("CanId.id.SrcAddr: %x\r\n",MyCAN_ID.CanStdId.SrcAddr);
+		printf("CanId.id.DstAddr: %x\r\n",MyCAN_ID.CanStdId.DstAddr);
+		*/
+	}
+	else
+	{
+		MyCAN_ID.CanExtId.DstAddr = MyDstIDAddr.CanIDAddr.StdIDAddr ;
+		MyCAN_ID.CanExtId.SrcAddr = MySrcIDAddr.CanIDAddr.StdIDAddr ;
+		MyCAN_ID.CanExtId.ExtDstAddr = MyDstIDAddr.CanIDAddr.ExtIDAddr;
+		MyCAN_ID.CanExtId.ExtSrcAddr = MySrcIDAddr.CanIDAddr.ExtIDAddr;
+		MyCAN_ID.CanExtId.FunCode = cmd | 0x10;
+		/*
+		printf("CanId.id.FunCode: %x\r\n",MyCAN_ID.CanExtId.FunCode);
+		printf("CanId.id.SrcAddr: %x\r\n",MyCAN_ID.CanExtId.SrcAddr);
+		printf("CanId.id.DstAddr: %x\r\n",MyCAN_ID.CanExtId.DstAddr);
+		*/
+	}
+
+	/* Transmit */
+	if(  CanRxCmdRingBuffer[CanRxCmdButtomCounter].IdType == CAN_ID_STD )
+	{
+		pMessage->StdId = MyCAN_ID.Id;
+	}
+	else
+	{
+		pMessage->ExtId = MyCAN_ID.Id;
+	}
+
+	pMessage->RTR = CAN_RTR_DATA;
+
+	pMessage->IDE = CanRxCmdRingBuffer[CanRxCmdButtomCounter].IdType;
+
+	pMessage->DLC = 8;
+}
+
+
+/******************************************************************************
+  Function:Can_Process
+  Description:
+  Input:None
+  Output:
+  Return:
+  Others:None
+******************************************************************************/
+void Can_Process( void )
+{
+	if(CanReceivecompleteFlg == 1)
+	{
+		if(CanRxCmdTopCounter != CanRxCmdButtomCounter)
+		{
+
+			Can_cmd_parse();
+
+			CanRxCmdButtomCounter++;
+
+			if(CanRxCmdButtomCounter == CANRINGBUFFERLEN)
+			{
+				CanRxCmdButtomCounter = 0;
+			}
+
+			if(CanRxCmdTopCounter == CanRxCmdButtomCounter)
+			{
+				 CanReceivecompleteFlg = 0;
+			}
+		}
+	}
+}
+
+
+/******************************************************************************
+  Function:Can_cmd_parse
+  Description:
+  Input:None
+  Output:
+  Return:
+  Others:None
+******************************************************************************/
+void Can_cmd_parse( void )
+{
+	//uint8_t CanCmdReturnFlg = 0;
+
+	//if((CanRxCmdRingBuffer[CanRxCmdButtomCounter].CanCmd & 0x10) == 0x10)
+		//CanCmdReturnFlg = 1;
+
+	switch(CanRxCmdRingBuffer[CanRxCmdButtomCounter].CanCmd & 0x0f)
+	{
+		case CAN_CMD_READ_AD:
+			//Can_return_ad_msg();
+			break;
+
+		case CAN_CMD_CHECK_BOARD:
+			//Can_return_board_msg();
+			break;
+
+		case CAN_CMD_RW_EEPROM:
+			//Can_return_eeprom_msg();
+			break;
+
+		case CAN_CMD_READ_SN_ID:
+			Can_return_sn_msg();
+		break;
+
+
+		default:
+			break;
+	}
+}
+
+/******************************************************************************
+  Function:Can_return_sn_msg
+  Description:
+  Input:None
+  Output:
+  Return:
+  Others:None
+******************************************************************************/
+uint32 Can_return_sn_msg( void )
+{
+	CanMsg CanToCanTxMessage;
+	uint32 status;
+
+	Can_change_return_id( &CanToCanTxMessage, CAN_CMD_READ_SN_ID );
+
+#ifdef USE_CAN_TEST
+	CanToCanTxMessage.Data[0] = 0x11;
+	CanToCanTxMessage.Data[1] = 0x22;
+	CanToCanTxMessage.Data[2] = 0x33;
+	CanToCanTxMessage.Data[3] = 0x44;
+	CanToCanTxMessage.Data[4] = 0x55;
+	CanToCanTxMessage.Data[5] = 0x66;
+	CanToCanTxMessage.Data[6] = 0x77;
+	CanToCanTxMessage.Data[7] = 0x88;
+#endif
+
+	status = CAN_Transmit(canREG3, 1, &CanToCanTxMessage);
+
+	return status;
+}
---
+++ TMS570LS1227_DEMO/source/sys_main.c
@@ -0,0 +1,123 @@
+/** @file sys_main.c 
+*   @brief Application main file
+*   @date 02-Mar-2016
+*   @version 04.05.02
+*
+*   This file contains an empty main function,
+*   which can be used for the application.
+*/
+
+/* 
+* Copyright (C) 2009-2016 Texas Instruments Incorporated - www.ti.com 
+* 
+* 
+*  Redistribution and use in source and binary forms, with or without 
+*  modification, are permitted provided that the following conditions 
+*  are met:
+*
+*    Redistributions of source code must retain the above copyright 
+*    notice, this list of conditions and the following disclaimer.
+*
+*    Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in the 
+*    documentation and/or other materials provided with the   
+*    distribution.
+*
+*    Neither the name of Texas Instruments Incorporated nor the names of
+*    its contributors may be used to endorse or promote products derived
+*    from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+*  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+*  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+*  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+*  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+*  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+*  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+*  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+*  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+
+
+/* USER CODE BEGIN (0) */
+#include "stdio.h"
+#include "sci.h"
+#include "sys_core.h"
+#include "can.h"
+#include "sys_vim.h"
+#include "CanProtocol.h"
+
+/* USER CODE END */
+
+/* Include Files */
+
+#include "sys_common.h"
+
+/* USER CODE BEGIN (1) */
+#define DELAY_VALUE           10000000
+#define CAN_DATA_BUFFER_LEN   64
+
+uint8 can_sent_buffer_data[CAN_DATA_BUFFER_LEN];
+uint8 can_receive_buffer_data[CAN_DATA_BUFFER_LEN*8];
+
+/* USER CODE END */
+
+/** @fn void main(void)
+*   @brief Application main function
+*   @note This function is empty by default.
+*
+*   This function is called after startup.
+*   The user can use this function to implement the application.
+*/
+
+/* USER CODE BEGIN (2) */
+/* USER CODE END */
+
+void main(void)
+{
+/* USER CODE BEGIN (3) */
+//    volatile unsigned long int i,j;
+//    uint32 status,tmpid,tmpIdType;
+//	CAN_EXTSTDIDTypedef CanId;
+
+	sciInit();
+	canInit();
+	canEnableErrorNotification(canREG3);
+//	canEnableloopback(canREG3,Internal_Lbk);
+
+	cansetIDType(canREG3, 2, CAN_ID_STD);
+/*
+	CanId.CanStdId.FunCode = 0x07;
+	CanId.CanStdId.SrcAddr = 0x01;
+	CanId.CanStdId.DstAddr = 0x02;
+	canSetID(canREG3, 1, CanId.Id);
+	cansetIDType(canREG3, 1, CAN_ID_STD);
+*/
+	vimInit();
+	_enable_interrupt_();
+/*
+	for (j = 0; j<CAN_DATA_BUFFER_LEN; j++)
+	{
+		can_sent_buffer_data[j] =  j;
+		can_receive_buffer_data[j] = 0;
+	}
+*/
+	while(1)
+	{
+		Can_Process();
+		/*
+		printf("Massage box%2d id = %8x idtype = %1d \r\n", 1, canGetID(canREG3, 1) , canGetIDType(canREG3, 1));
+		printf("Massage box%2d id = %8x idtype = %1d \r\n", 2, canGetID(canREG3, 2) , canGetIDType(canREG3, 2));
+		status = canTransmit(canREG3, 1, &(can_sent_buffer_data[0]));
+		printf("Massage box%2d id = %8x idtype = %1d canTransmit return status = %4d \r\n", 1, tmpid>>18, tmpIdType, status);
+		for (i = 0; i < DELAY_VALUE; i++);
+		*/
+	}
+/* USER CODE END */
+}
+
+/* USER CODE BEGIN (4) */
+/* USER CODE END */
