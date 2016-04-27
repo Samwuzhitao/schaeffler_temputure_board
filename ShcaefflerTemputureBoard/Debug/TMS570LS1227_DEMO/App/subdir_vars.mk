ade29d12c1e4bad3e01d39ce3b0692954933b1bb
samwu <sam.wu@embest-tech.com>
2016-04-27 09:01:52 +0800

    Project:Setting:Add project file
    Signed-off-by: samwu <sam.wu@embest-tech.com>

---
+++ ShcaefflerTemputureBoard/Debug/TMS570LS1227_DEMO/App/subdir_vars.mk
@@ -0,0 +1,60 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../TMS570LS1227_DEMO/App/ADS1247.c \
+../TMS570LS1227_DEMO/App/CanProtocol.c \
+../TMS570LS1227_DEMO/App/DS2431.c \
+../TMS570LS1227_DEMO/App/LED.c \
+../TMS570LS1227_DEMO/App/OneWire.c \
+../TMS570LS1227_DEMO/App/Version.c \
+../TMS570LS1227_DEMO/App/printf.c 
+
+OBJS += \
+./TMS570LS1227_DEMO/App/ADS1247.obj \
+./TMS570LS1227_DEMO/App/CanProtocol.obj \
+./TMS570LS1227_DEMO/App/DS2431.obj \
+./TMS570LS1227_DEMO/App/LED.obj \
+./TMS570LS1227_DEMO/App/OneWire.obj \
+./TMS570LS1227_DEMO/App/Version.obj \
+./TMS570LS1227_DEMO/App/printf.obj 
+
+C_DEPS += \
+./TMS570LS1227_DEMO/App/ADS1247.pp \
+./TMS570LS1227_DEMO/App/CanProtocol.pp \
+./TMS570LS1227_DEMO/App/DS2431.pp \
+./TMS570LS1227_DEMO/App/LED.pp \
+./TMS570LS1227_DEMO/App/OneWire.pp \
+./TMS570LS1227_DEMO/App/Version.pp \
+./TMS570LS1227_DEMO/App/printf.pp 
+
+C_DEPS__QUOTED += \
+"TMS570LS1227_DEMO\App\ADS1247.pp" \
+"TMS570LS1227_DEMO\App\CanProtocol.pp" \
+"TMS570LS1227_DEMO\App\DS2431.pp" \
+"TMS570LS1227_DEMO\App\LED.pp" \
+"TMS570LS1227_DEMO\App\OneWire.pp" \
+"TMS570LS1227_DEMO\App\Version.pp" \
+"TMS570LS1227_DEMO\App\printf.pp" 
+
+OBJS__QUOTED += \
+"TMS570LS1227_DEMO\App\ADS1247.obj" \
+"TMS570LS1227_DEMO\App\CanProtocol.obj" \
+"TMS570LS1227_DEMO\App\DS2431.obj" \
+"TMS570LS1227_DEMO\App\LED.obj" \
+"TMS570LS1227_DEMO\App\OneWire.obj" \
+"TMS570LS1227_DEMO\App\Version.obj" \
+"TMS570LS1227_DEMO\App\printf.obj" 
+
+C_SRCS__QUOTED += \
+"../TMS570LS1227_DEMO/App/ADS1247.c" \
+"../TMS570LS1227_DEMO/App/CanProtocol.c" \
+"../TMS570LS1227_DEMO/App/DS2431.c" \
+"../TMS570LS1227_DEMO/App/LED.c" \
+"../TMS570LS1227_DEMO/App/OneWire.c" \
+"../TMS570LS1227_DEMO/App/Version.c" \
+"../TMS570LS1227_DEMO/App/printf.c" 
+
+
